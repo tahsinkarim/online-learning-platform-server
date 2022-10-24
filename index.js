7204c14b01769cd2eee5bdbed386819715a61209
tahsinkarim <tahsinnkarim@gmail.com>
2022-10-24 22:53:27 +0600

    Updated id

--- index.js
+++ index.js
@@ -7,13 +7,22 @@ app.use(cors());
 const Port = process.env.Port || 5000;
 
 const courses = require('./Data/courses.json')
+const course = require('./Data/coursedata.json')
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/courses', (req, res) => {
     res.send(courses)
-  })
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id
+    const selectedCourse = course.find(e => e.course_id === id)
+    res.send(selectedCourse)
+})
+
 
 app.listen(Port, () => {
   console.log(`Example app listening on port ${Port}`)
